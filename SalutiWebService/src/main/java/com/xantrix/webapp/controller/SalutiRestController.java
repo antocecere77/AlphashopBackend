5bd30c8b88c10b956a63e90bfe8f701d29bb66e9
Antonio Cecere <antocecere77@gmail.com>
2019-07-14 16:46:40 +0200

    Aggiunta abilitazione CORS su porta 4200

--- SalutiWebService/src/main/java/com/xantrix/webapp/controller/SalutiRestController.java
+++ SalutiWebService/src/main/java/com/xantrix/webapp/controller/SalutiRestController.java
@@ -1,5 +1,6 @@
 package com.xantrix.webapp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200")
 public class SalutiRestController {
 	
 	@GetMapping(value = "/test")
