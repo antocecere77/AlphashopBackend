c8fcc3767a8a4381a26452930b2572be41882212
Antonio Cecere <antocecere77@gmail.com>
2019-07-30 07:00:19 +0200

    Aggiunta configurazione api per ruolo

--- ArticoliWebService/src/main/java/com/xantrix/webapp/security/SecurityConfiguration.java
+++ ArticoliWebService/src/main/java/com/xantrix/webapp/security/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.xantrix.webapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +16,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	private static String REALM = "REAME";
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -39,6 +43,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return manager;
 	}
 	
+	private static final String[] USER_MATCHER = { "/api/articoli/cerca/**"};
+	private static final String[] ADMIN_MATCHER = { "/api/articoli/inserisci/**", 
+			"/api/articoli/modifica/**", "/api/articoli/elimina/**" };
 	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.authorizeRequests()
+				.antMatchers(USER_MATCHER).hasAnyRole("USER")
+				.antMatchers(ADMIN_MATCHER).hasAnyRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	public AuthEntryPoint getBasicAuthEntryPoint() {
+		return new AuthEntryPoint();
+	}
 	
 }
